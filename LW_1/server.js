087bc880e77e01f31cb5cf40762d519b752ceb94
Natan Nedaikhlib <natan.nedaikhlib@gmail.com>
2026-02-18 20:17:36 +0100

    Taks2: Modify server.js from task1 for logging http-method and url from user.

--- LW_1/server.js
+++ LW_1/server.js
@@ -1,10 +1,15 @@
 const http = require("http");
 
-const PORT = 4000;
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+    const method = req.method;
+    const url = req.url;
+
+    console.log(`${new Date().toISOString()} ${method} ${url}`);
+
     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8"});
-    res.end("Hello, Server! Made by Natan Nedaikhlib.");
+    res.end(`You made a ${method} request to ${url}`);
 });
 
 server.listen(PORT, () => {
